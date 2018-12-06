deb0e20bab8e0755feeec296b30aadd64640c6e0
이예현 <shszyh@iclooud.com>
2018-12-06 20:07:51 +0900

    RETRY

---
+++ movie.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "movie.h"
+
+typedef struct movInfo{
+	char name[200];
+	float score;
+	int runTime;
+	char madeIn[10];
+} movInfo_t;
+
+
+void* mv_genMvInfo(char* name, float score, int runTime, char* country)
+{
+	movInfo_t* mvPtr;
+	
+	//allocate memory and set the member variables
+	
+	return (void*)mvPtr;
+}
+
+void mv_print(void* obj)
+{
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
+	
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	
+	return;
+}
+
+
+//return the score value from the input instance of movInfo_t structure
+float mv_getScore(void* obj)
+{
+	
+}
+
+//return the runtime value from the input instance of movInfo_t structure
+int mv_getRunTime(void* obj)
+{
+	
+}
+
+//return the name string pointer from the input instance of movInfo_t structure
+char* mv_getName(void* obj)
+{
+	
+}
+
+//return the country string pointer from the input instance of movInfo_t structure
+char* mv_getCountry(void* obj)
+{
+	
+}
+
+
---
+++ movie.h
@@ -0,0 +1,7 @@
+
+void* mv_genMvInfo(char* name, float score, int runTime, char* country);
+void mv_print(void* obj);
+float mv_getScore(void* obj);
+int mv_getRunTime(void* obj);
+char* mv_getName(void* obj);
+char* mv_getCountry(void* obj);
